fff30dfbb5b026dcdb2386f90e29074cc8c95ac4
domgeorge <obinnadom007@gmail.com>
2023-08-10 20:24:07 -0700

    type sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function starts
+ *
+ * Return: success returns 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)", sizeof(char));
+	printf("Size of an int: %lu bytes(s)", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)", sizeof(float));
+
+	return (0);
+}

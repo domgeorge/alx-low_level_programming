d0e0b0fbd995a4e4d7912ce866de38c0ddc00bef
domgeorge <obinnadom007@gmail.com>
2023-08-11 16:01:59 -0700

    Updated

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: exit 0 success
+ */
+
+int main(void)
+{
+	char a;
+	char b;
+
+	a = 48;
+	b = 97;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 102)
+	{
+		putchar(b);
+		b++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}

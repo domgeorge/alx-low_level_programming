ad0781043240dad41ce164239ec5add4a2cfffeb
domgeorge <obinnadom007@gmail.com>
2023-08-11 16:45:48 -0700

    Advance

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: exits success 0
+ */
+
+int main(void)
+{
+	int a = '0';
+	int b;
+
+	while (a <= '9')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			putchar(a);
+			putchar(b);
+			if (a == '8' && b == '9')
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			
+			b++;
+		}
+
+		a++;
+	}
+
+	return (0);
+}

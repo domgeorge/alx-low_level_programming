b39656e9b769770ca5f87a7fc380db5efec078c8
domgeorge <obinnadom007@gmail.com>
2023-08-15 15:10:00 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets from a to z
+ * Return: exits 0 success
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for(c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}

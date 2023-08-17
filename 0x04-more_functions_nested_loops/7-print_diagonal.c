1908b755c2db1e3b4bfab32a7f19edc592e93678
domgeorge <obinnadom007@gmail.com>
2023-08-17 14:13:46 -0700

    Forward slash

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - outputs diagonal lines according
+ * to the integer
+ * @n: argument for integer
+ */ 
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			for (i = 1; i <= j; i++)
+			{
+				if (i != 1)
+					_putchar(' ');
+				if (i == j)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	else
+		_putchar('\n');
+}

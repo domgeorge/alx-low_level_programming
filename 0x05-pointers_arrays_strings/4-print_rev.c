14235eefc9ab4f3cfb2b0f390e5c836a26487f95
domgeorge <obinnadom007@gmail.com>
2023-08-21 15:11:49 -0700

    Updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,18 @@
 
 void print_rev(char *s)
 {
-	int i;
-
+	int i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	i--;
-	while (s[i] != '\0')
+
+	i = i - 1;
+
+	while (i >= 0)
 	{
 		_putchar(s[i--]);
 	}
+
 	_putchar('\n');
 }

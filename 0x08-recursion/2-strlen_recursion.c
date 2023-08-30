0ea46187425bb3ac2addd72c718d3583dde5b78a
domgeorge <obinnadom007@gmail.com>
2023-08-30 14:39:48 -0700

    Betty style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,15 +3,15 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: pointer to the character
- * Return: base case and end of string
- * Return: increment by 1 and call recursively
+ * Return: base case ie end of string and increment by
+ * 1 whiile calling recursively
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return (0);  
+		return (0);
 	}
 
 	return (1 + _strlen_recursion(s + 1));

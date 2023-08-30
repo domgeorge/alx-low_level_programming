f0448b07eeaa603f96f409d61f272b2310f677af
domgeorge <obinnadom007@gmail.com>
2023-08-30 10:07:03 -0700

    Betty checked

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to character
+ * Return: ends the recursion
+ */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')

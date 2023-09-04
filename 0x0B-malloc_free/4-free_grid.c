6335774f6fb4535dbfcb37b75e6259114cbabd31
domgeorge <obinnadom007@gmail.com>
2023-09-04 16:36:32 -0700

    Deallocate

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * free_grid -  frees a 2 dimensional grid previously created
+ * @grid: grid pointer
+ * @height: takes input of integer
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid != NULL)
+	{
+		while (i < height)
+		{
+			free(gird[i]);
+		}
+
+		free(grid);
+	}
+}

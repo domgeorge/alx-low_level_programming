fbe5e5081c2b5a8747181eb94bc82ad3a214d80e
domgeorge <obinnadom007@gmail.com>
2023-09-13 14:20:57 -0700

    Function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_name -  prints a name
+ * @name: pointer to char
+ * @f: pointer to function of type char
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCPTS_H
+#define FUNCPTS_H
+
+void print_name(char *name, void (*f)(char *));
+
+
+#endif

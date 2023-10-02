03afe1a6fb49a3bd32f3ee3823286d8b0cb5283e
domgeorge <obinnadom007@gmail.com>
2023-10-02 13:12:49 -0700

    Read text file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef FILEDESC_H
+#define FILEDESC_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif

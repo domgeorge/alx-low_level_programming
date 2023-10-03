9f964c637703379c5b0da8b267d4e465c6c1f9ed
domgeorge <obinnadom007@gmail.com>
2023-10-02 17:54:00 -0700

    Copy file content

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <errno.h>
+
+#define BUFSIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
